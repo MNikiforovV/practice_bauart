1cbd6ff4d3bca5ca64518d3e37f923c2f7ff20ab
FCRiajinka <isaevmishuta@gmail.com>
2022-06-27 11:58:29 +0700

    front (registration/auth)
    Создал регистрацию и авторизацию

--- front/hello-world/src/App.vue
+++ front/hello-world/src/App.vue
@@ -1,28 +1,82 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-sm-3 mx-auto">
+        <form @submit.prevent="registerUser" novalidate>
+
+          <div v-show= "step ===1" class="step">
+            <div class="mb-3">
+              <label for="name" class="form-label">Имя</label>
+              <input type="text" class="form-control" id="name" placeholder="Ваше имя">
+            </div>
+
+            <div class="mb-3">
+              <label for="surname" class="form-label">Фамилия</label>
+              <input type="text" class="form-control" id="surname" placeholder="Ваша фамилия">
+            </div>
+            
+            <div class="mb-3">
+              <label for="email" class="form-label">Электронная почта</label>
+              <input type="email" class="form-control" id="email" placeholder="Ваша почта">
+            </div>
+
+            <div class="mb-3"></div>
+            <button @click="nextStep" type="button" class="btn btn-primary">Следующий шаг</button>
+          </div>
+
+            
+          <transition name="slide-fade">
+            <div v-show= "step ===2" class="step">
+              <div class="mb-3">
+                <label for="password" class="form-label">Пароль</label>
+                <input type="password" class="form-control" id="password">
+              </div>
+
+              <div class="mb-3">
+                <label for="passwordConfirm" class="form-label">Подтверждение пароля</label>
+                <input type="password" class="form-control" id="passwordConfirm">
+              </div>
+
+              <button @click="backStep" type="button" class="btn btn-light" style="margin-right: 10px">Назад</button>
+              <button type="submit" class="btn btn-primary">Зарегестрироваться</button>
+              
+            </div>
+          </transition>
+
+        </form>
+      </div>
+    </div> 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  data () {
+    return {
+      step: 1
+    }
+  },
+  methods: {
+    nextStep() {
+      if(this.step < 2) {
+        this.step++
+      }
+    },
+    backStep () {
+      if(this.step > 1 ) {
+        this.step--
+      }
+    },
+    registerUser () {
+      console.log ('Регистрация прошла успешно!')
+    }
   }
 }
+ 
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
+
 </style>
